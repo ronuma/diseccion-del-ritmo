809db426a7314d4bfaa3037222f780c332421857
Rodrigo Nuñez <ronum.00@gmail.com>
2024-10-25 18:04:12 -0600

    Finished deliverable for second version

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -30,53 +30,16 @@ func _process(_delta: float) -> void:
 func handle_input(node: AudioStreamPlayer):
 	if node.playing:
 		node.stop()
-		assign_new_ref(node)
 	else:
 		node.play(ref_player.get_playback_position())
+	assign_new_ref(node)
 		
 func assign_new_ref(node: AudioStreamPlayer):
-	# Assign a ref if node that was stopped was the current ref
-	if ref_player != node:
-		return
-	else:
-		for item in audio_data:
-			if item.node.playing:
-				ref_player = item.node
-				break
-				
+	# Ensure ref is always a playing instrument
+	for item in audio_data:
+		if item.node.playing:
+			ref_player = item.node
+			break	
 func _on_button_pressed() -> void:
 	# Change scene to live when button is pressed
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-	
-	
-	
-	
-
-#var score = 0
-#@onready var score_label = $"../ScoreLabel"
-#@onready var bg = $"../BG"
-	
-#var LOOP_DURATION = globals.LOOP_DURATION
-#var BAR_DURATION = LOOP_DURATION / 2
-#const PASS_RANGE = 0.05
-#const MAX_IMG_MODULATION = 0.7
-		
-#func transform_value(val: float):
-	## Transform value to alter image's appearance
-	#val = val if val < BAR_DURATION else val - BAR_DURATION
-	#return abs(MAX_IMG_MODULATION - min(val, abs(val - BAR_DURATION)))
-			#
-#func checkScore(val: float):
-	## Check if value is valid and "in time"
-	#if val <= PASS_RANGE || val >= LOOP_DURATION - PASS_RANGE || (val <= LOOP_DURATION / 2 + PASS_RANGE && val >= LOOP_DURATION / 2 - PASS_RANGE):
-		#return true
-	#else:
-		#return false
-		#if checkScore(ref_time):
-		#score += 1
-		#score_label.text = "Aciertos: " + str(score)
-		#bg.modulate = Color(1,1,1)
-	## Time is not on the valid range so it can be used as parameter
-	#else:
-		#var v = transform_value(ref_time)
-		#bg.modulate = Color(v,v,v)
